c34867b734c779f78f67607d01da5d35d5ac4dd7
RobSaidov <robsaidov@gmail.com>
2024-09-13 15:21:48 -0700

    imported necessary modules 2.0

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#import modules
+#Importing modules
 
-import tensorflow as tf
-import matplotlib.pyplot as plt
+import tesorflow as tf
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2 as cv
-import tensorflow
-
-cd C:\Users\user\PycharmProjects\EmoSense
+from tensorflow import keras
+from tensorflow.keras import layers, models
